2d26f420862d54efd2c64c586c2f37b99fa09732
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-19 18:44:38 -0500

    Update main.go
    commit to put other dice in. just added all in the commit.

--- main.go
+++ main.go
@@ -2,99 +2,158 @@
 package main
 
 import (
+    "database/sql"
+    "log"
+    "net/http"
+    "text/template"
+    
+    _ "github.com/go-sql-driver/mysql"
+    )
 
-    "fmt"
-    "os"
-	"os/signal"
-    "github.com/bwmarrin/discordgo"
-    "syscall"
-    "math/rand"
-    "sync"
-    "time"
-    "strconv"
-)
+type Employee struct {
+    Id int
+    Name string
+    City string
+}
 
-const token string 
-var BotID string
+func dbConn() (db *sql.DB) {
+    dbDriver := "mysql"
+    dbUser := "root"
+    dbPass := "Tobi@s0506"
+    dbName := "goblog"
+    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+    if err != nil {
+        panic(err.Error())
+    }
+    return db
+}
 
+var tmpl = template.Must(template.ParseGlob("form/*"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    selDB, err := db.Query("SELECT * FROM Employee ORDER BY id DESC")
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Employee{}
+    res := []Employee{}
+    for selDB.Next() {
+        var id int
+        var name, city string
+        err = selDB.Scan(&id, &name, &city)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = id
+        emp.Name = name
+        emp.City = city
+        res = append(res, emp)
+    }
+    tmpl.ExecuteTemplate(w, "Index", res)
+    defer db.Close()
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    nId := r.URL.Query().Get("id")
+    selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Employee{}
+    for selDB.Next() {
+        var id int
+        var name, city string
+        err = selDB.Scan(&id, &name, &city)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = id
+        emp.Name = name
+        emp.City = city
+    }
+    tmpl.ExecuteTemplate(w, "Show", emp)
+    defer db.Close()
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+    tmpl.ExecuteTemplate(w, "New", nil)
+}
+func Edit(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    nId := r.URL.Query().Get("id")
+    selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Employee{}
+    for selDB.Next() {
+        var id int
+        var name, city string
+        err = selDB.Scan(&id, &name, &city)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = id
+        emp.Name = name
+        emp.City = city
+    }
+    tmpl.ExecuteTemplate(w, "Edit", emp)
+    defer db.Close()
+}
+func Insert (w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    if r.Method == "POST" {
+        name := r.FormValue("name")
+        city := r.FormValue("city")
+        insForm, err := db.Prepare("INSERT INTO Employee(name, city) VALUES(?,?)")
+        if err !=nil {
+            panic(err.Error())
+        }
+        insForm.Exec(name, city)
+        log.Println("INSERT: Name: " + name + " | City: " + city)
+    }
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
+func Update(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    if r.Method == "POST" {
+        name := r.FormValue("name")
+        city := r.FormValue("city")
+        id := r.FormValue("uid")
+        insForm, err := db.Prepare("UPDATE Employee SET name=?, city=? WHERE id=?")
+        if err !=nil {
+            panic(err.Error())
+        }
+        insForm.Exec(name, city, id)
+        log.Println("UPDATE: Name: " + name + " | City: " + city)
+    }
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
+            
+func Delete(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    emp := r.URL.Query().Get("id")
+    delForm, err := db.Prepare("DELETE FROM Employee WHERE id=?")
+    if err != nil{
+        panic(err.Error())
+    }
+    delForm.Exec(emp)
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
 
 func main() {
-	dg, err := discordgo.New("Bot " + token)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	u, err := dg.User("@me")
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	BotID = u.ID
-
-	dg.AddHandler(messageHandler)
-
-	err = dg.Open()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println("Bot is running!")
-    sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
-
-	// Cleanly close down the Discord session.
-	dg.Close()
-    return
-}
-
-
-
-var onlyOnce sync.Once
-
-//prepare d6\20 dice
-var diceTwenty = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                      11, 12, 13, 14, 15, 16, 17, 18,
-                      19, 20}
-
-func rollDiceTwenty() int {
-    
-    onlyOnce.Do(func() {
-        rand.Seed(time.Now().UnixNano()) //this runs once
-    })
-    
-    return diceTwenty[rand.Intn(len(diceTwenty))]
-}
-
-func advrollDiceTwenty() int {
-    
-    onlyOnce.Do(func() {
-        rand.Seed(time.Now().UnixNano()) //this runs once
-    })
-    
-    return diceTwenty[rand.Intn(len(diceTwenty))]
-}
-
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
- 
-dice20 := rollDiceTwenty()
-    d20 := strconv.Itoa(dice20)
-    advdice20 := advrollDiceTwenty()
-    advd20 := strconv.Itoa(advdice20)
-    if m.Author.ID == BotID {
-        return
-    }
-    
-    if m.Content == "!roll" {
-        _, _ = s.ChannelMessageSend("678667262437228544", "D20: " + d20)
-    }
-    
-    if m.Content == "!advroll" {
-        _, _ = s.ChannelMessageSend("678667262437228544", "D20: " + d20 + " D20: " + advd20)
-    }
+    log.Println("Server started on: http://localhost:8080")
+    http.HandleFunc("/", Index)
+    http.HandleFunc("/show", Show)
+    http.HandleFunc("/new", New)
+    http.HandleFunc("/edit", Edit)
+    http.HandleFunc("/insert", Insert)
+    http.HandleFunc("/update", Update)
+    http.HandleFunc("/delete", Delete)
+    http.ListenAndServe(":8080", nil)
 }
